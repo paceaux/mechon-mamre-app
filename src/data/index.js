932aa3b2d9472c69b6a8069881d4924cd6c9c954
Frank Taylor <Frank.Taylor@exlrt.com>
2019-08-09 10:40:37 -0500

    adds data

---
+++ src/data/index.js
@@ -0,0 +1,60 @@
+import * as Genesis from './Genesis.json';
+import * as Exodus from './Exodus.json';
+import * as Leviticus from './Leviticus.json';
+import * as Numbers from './Numbers.json';
+import * as Deuteronomy from './Deuteronomy.json';
+
+import * as Joshua from './Joshua.json';
+import * as Judges from './Judges.json';
+import * as Samuel from './Samuel.json';
+import * as Kings from './Kings.json';
+import * as Isaiah from './Isaiah.json';
+import * as Jeremiah from './Jeremiah.json';
+import * as Ezekiel from './Ezekiel.json';
+import * as Joel from './Joel.json';
+import * as Hosea from './Hosea.json';
+import * as Amos from './Amos.json';
+import * as Obadiah from './Obadiah.json';
+import * as Jonah from './Jonah.json';
+import * as Micah from './Micah.json';
+import * as Nahum from './Nahum.json';
+import * as Habakkuk from './Habakkuk.json';
+import * as Zephaniah from './Zephaniah.json';
+import * as Haggai from './Haggai.json';
+import * as Zechariah from './Zechariah.json';
+import * as Malachi from './Malachi.json';
+
+const Torah = {
+  Genesis,
+  Exodus,
+  Leviticus,
+  Numbers,
+  Deuteronomy,
+};
+
+const Prophets = {
+  Joshua,
+  Judges,
+  Samuel,
+  Kings,
+  Isaiah,
+  Jeremiah,
+  Ezekiel,
+  Hosea,
+  Joel,
+  Amos,
+  Obadiah,
+  Jonah,
+  Micah,
+  Nahum,
+  Habakkuk,
+  Zephaniah,
+  Haggai,
+  Zechariah,
+  Malachi,
+};
+
+export {
+  Torah,
+  Prophets,
+};
