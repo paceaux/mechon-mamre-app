60eebae50805fcc5b21533df3916173febb143b7
Frank Taylor <Frank.Taylor@exlrt.com>
2019-08-29 12:29:12 -0500

    removes unnec classname

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <GlobalHeader class="g-header__hgroup" msg="Mechon Mamre"/>
+    <GlobalHeader msg="Mechon Mamre"/>
     <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
